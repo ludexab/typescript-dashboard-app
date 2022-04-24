6d46bd50b6112bf4193f7ff83c7cfce9eb6a24d7
boss <ileanwaabalaka@gmail.com>
2022-04-24 19:42:05 +0100

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,59 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid";
 
-import confetti from 'canvas-confetti';
+let input = document.querySelector<HTMLInputElement>("#input-project");
+let form = document.getElementById("project-form") as HTMLFormElement | null;
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+type Project = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+  deadline: Date;
+};
+
+const saveProjects = () => {
+  localStorage.setItem("PROJECTS", JSON.stringify(allProjects));
+};
+
+const loadProjects = (): Project[] => {
+  const projectJSON = localStorage.getItem("PROJECTS");
+  if (projectJSON == null) return [];
+  return JSON.parse(projectJSON);
+};
+
+const addProject = (project: Project) => {
+  let ul = document.querySelector<HTMLUListElement>("#projects");
+  const li = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.addEventListener("change", () => {
+    project.completed = checkbox.checked;
+    saveProjects();
+  });
+  checkbox.checked = project.completed;
+  label.append(checkbox, project.title);
+  li.append(label);
+  ul?.append(li);
+};
+
+const allProjects: Project[] = loadProjects();
+allProjects.forEach(addProject);
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input?.value == "" || input?.value == null) return;
+  const newProject = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+    deadline: new Date(),
+  };
+
+  allProjects.push(newProject);
+  saveProjects();
+
+  addProject(newProject);
+  input.value = "";
+});
